7b51d5abb681056fd363b0636064a71402de883a
Cesar Alcantara <cja5391@psu.edu>
2021-09-02 16:04:20 -0400

    part one of creating browse UI done

--- SpotifyMock/Controllers/Core/HomeViewController.swift
+++ SpotifyMock/Controllers/Core/HomeViewController.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection? in
+        return createSectionLayout(section: sectionIndex)
+    })
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        return spinner
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,13 +24,56 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         // adding gear icon on nav bar to get to settings UI
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettings))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "gear"),
+            style: .done, target: self,
+            action: #selector(didTapSettings))
+        
+        // configure collection view
+        configureCollectionView()
         
         // fetching api data
         fetchData()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    private func configureCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+    }
+    
+    private static func createSectionLayout(section: Int) -> NSCollectionLayoutSection {
+        // item
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)))
+        
+        // group
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(120)),
+            subitem: item, count: 1)
+        
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+    }
+    
     private func fetchData() {
+        // Featured Playlist
+        // Recommended Tracks
+        // New Releases
+        
         APICaller.shared.getRecommendationsGenres { result in
             switch result {
             case .success(let model):
@@ -51,7 +103,6 @@ class HomeViewController: UIViewController {
 //            }
 //        }
     
-    
     // settings handler
     @objc func didTapSettings() {
         let vc = SettingsViewController()
@@ -59,5 +110,24 @@ class HomeViewController: UIViewController {
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
     }
+}
+
+// MARK: - Extensions
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // MARK: - Data Source Functions
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    
+    
+    // MARK: - Delegate Functions
 }
 
